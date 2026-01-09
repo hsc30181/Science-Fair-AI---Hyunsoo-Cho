f2f528bd725d6dbef569603c80a538f5b2a120ae
jchs <s30181@asmadrid.es>
2026-01-09 15:14:11 +0100

    Implement image preprocessing in app.py
    Added image preprocessing function for model input.

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, redirect, request
 import tensorflow as tf
+import numpy as np
+from PIL import Image
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "uytrfdcvbnjuytresdes"
+
+IMG_SIZE = 224
+
+def preprocess_image(img_path):
+    img = Image.open(img_path).convert("RGB")
+    img.resize((IMG_SIZE, IMG_SIZE))
+    img = np.array(img) / 255.0
+    return np.expand_dims(img, axis=0)
 
 @app.route("/")
 def index():
